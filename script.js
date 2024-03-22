22a52343855b8999e4bd1e37460c3bff84af7120
Sanskarr25 <sanskarshete225@gmail.com>
2024-03-22 12:14:04 +0530

    Added first javaScript functionality . Change color of navbar when scrolling down.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+let navbar = document.querySelector("nav");
+let scrolled = false;
 
+window.onscroll = function () {
+  if (window.scrollY > 50 && !scrolled) {
+    navbar.style.backgroundColor = ""; // Change color on scroll
+    scrolled = true;
+  } else if (window.scrollY <= 50 && scrolled) {
+    navbar.style.backgroundColor = "white"; // Reset to initial color
+    scrolled = false;
+  }
+};
